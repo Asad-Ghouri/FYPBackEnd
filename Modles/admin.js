ab86510f410a71e5e0f186cd432a0908420394f5
Asad-Ghouri <asadghouri546@gmail.com>
2024-05-15 12:37:22 +0500

    .

---
+++ Modles/admin.js
@@ -0,0 +1,21 @@
+// models/Admin.js
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  commissionRate: { type: Number,
+    default: 3,
+  },
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;

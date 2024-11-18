10cebbac17695a3d485ee5371e4321af8e780284
Asad-Ghouri <asadghouri546@gmail.com>
2024-11-19 03:16:45 +0500

    .

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongose = require("mongoose");
 
 
-const db = "mongodb+srv://asadghouri546:7Oj4ABEpgn93Is9Z@cluster0.wirmp0f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const db = "mongodb+srv://asadghouri546:asadghouri546@cluster0.wirmp0f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongose.connect(db , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const app =express();
-const doteenv =require('dotenv');
+const cors = require('cors'); // Import cors
+const dotenv = require('dotenv');
 
-doteenv.config({path: './config.env'});
+const app = express();
+
+dotenv.config({ path: './config.env' });
 require('./DB/connection');
+
 const port = 5000;
 
+// Enable CORS for all routes
+app.use(cors({
+    origin: '*', // Allow this specific origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies and other credentials
+}));
+
+// Parse JSON
 app.use(express.json());
+
+// Use routes
 app.use(require('./Router/auth.js'));
 
-
-
-app.listen(port,() =>{
-    console.log(`Sever is running at localhost ${port}`);  
-})
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at localhost:${port}`);
+});

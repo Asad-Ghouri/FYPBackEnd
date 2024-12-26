8c17f15a852b064a7a0d5532a9dffbd3eb2ba193
Asad-Ghouri <asadghouri546@gmail.com>
2024-12-27 02:31:41 +0500

    cors issue resolved

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const port = 5000;
 // Connect to the database before starting the server
 (async () => {
   await connectDB(); // Await the database connection
+
+  app.use(cors({
+    origin: [
+      'http://localhost:3000', // Local frontend development
+      'https://fyp-back-end-bay.vercel.app' // Replace with your Vercel app URL
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies if needed
+  }));
+
   app.use(express.json());
   app.use(cors());
   app.use('/api', require('./api/auth.js'));
